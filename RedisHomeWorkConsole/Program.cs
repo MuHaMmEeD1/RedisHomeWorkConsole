78cc5467744c339bca645b0f50c8410ded9ae2f5
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2024-12-12 15:06:23 +0400

    add
    add

---
+++ RedisHomeWorkConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Queues;
+using RedisHomeWorkConsole.Types;
+using System.Text.Json;
+using System.Threading;
+
+string jsonFilePath = @"../../../appsettings.json";
+string jsonString = File.ReadAllText(jsonFilePath);
+
+AzureQueueType azureQueue = JsonSerializer.Deserialize<AzureQueueType>(jsonString);
+
+QueueClient queueClient = new QueueClient(azureQueue.ConnectionString, azureQueue.QueueName);
+await queueClient.CreateIfNotExistsAsync();
+
+while (true)
+{
+    Console.Write("Filim Adi Daxil Edin:");
+    string filimName = Console.ReadLine();
+    Console.Clear();
+
+    if (!string.IsNullOrWhiteSpace(filimName))
+    {
+        try
+        {
+            Console.WriteLine($"Filim: {filimName} <- Elave edildi");
+
+            await queueClient.SendMessageAsync(filimName);
+            Console.WriteLine("Message Getdi ok.");
+
+            Thread.Sleep(1400);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata oluştu: {ex.Message}");
+            Thread.Sleep(1400);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Filim Adi Bos Ola Bilmez!!!!");
+        Thread.Sleep(1400);
+    }
+
+    Console.Clear();
+}
